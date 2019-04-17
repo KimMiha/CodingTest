8505b861fac5ce678123b1d3d8b0b61a4d09cde0
KimMiha <dev.mihakim@gmail.com>
2019-04-17 20:19:45 +0900

    HackerRank - Practice - Tutorials - 10 Days of Statistics - Day 0: Weighted Mean

---
+++ src/hackerrank/practice/tutorials/tenDaysOfStatistics/day0WeightedMean/Solution.java
@@ -0,0 +1,29 @@
+package hackerrank.practice.tutorials.tenDaysOfStatistics.day0WeightedMean;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/s10-weighted-mean/problem
+public class Solution {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int size = scanner.nextInt();
+    int[] xArr = new int[size];
+    int[] wArr = new int[size];
+    double x = 0.0;
+    double w = 0.0;
+
+    for (int i = 0; i < size; i++) {
+      xArr[i] = scanner.nextInt();
+    }
+
+    for (int i = 0; i < size; i++) {
+      wArr[i] = scanner.nextInt();
+      x += xArr[i] * wArr[i];
+      w += wArr[i];
+    }
+
+    System.out.printf("%.1f", x / w);
+  }
+}
