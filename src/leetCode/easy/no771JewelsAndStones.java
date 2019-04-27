c2ce7036c66d11bb42e11508eacaa8a50e1d4871
KimMiha <dev.mihakim@gmail.com>
2019-04-27 17:26:19 +0900

    LeetCode 771. Jewels and Stones

---
+++ src/leetCode/easy/no771JewelsAndStones.java
@@ -0,0 +1,24 @@
+package leetCode.easy;
+
+public class no771JewelsAndStones {
+
+  public int numJewelsInStones(String J, String S) {
+    char[] jewels = J.toCharArray();
+    char[] stones = S.toCharArray();
+    int count = 0;
+
+    for(char s : stones){
+      for(char j : jewels){
+        if(s == j)
+          count++;
+      }
+    }
+    return count;
+  }
+
+}
+
+/* Result Details
+Runtime: 1 ms, faster than 98.59% of Java online submissions for Jewels and Stones.
+Memory Usage: 36.9 MB, less than 87.95% of Java online submissions for Jewels and Stones.
+ */
