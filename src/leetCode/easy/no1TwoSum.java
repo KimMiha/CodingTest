cd920c7f534c90f6b5d02b218d441385243138ab
KimMiha <dev.mihakim@gmail.com>
2020-01-02 23:24:20 +0900

    LeetCode Ease 1. Two Sum

---
+++ src/leetCode/easy/no1TwoSum.java
@@ -0,0 +1,25 @@
+package leetCode.easy;
+
+public class no1TwoSum {
+
+  public int[] twoSum(int[] nums, int target) {
+    int result[] = {0, 0};
+
+    for (int i = 0; i < nums.length; i++) {
+      int hope = target - nums[i];
+      for (int j = 0; j < nums.length; j++) {
+        if (nums[j] > hope || i == j) continue;
+        if (nums[j] == hope) {
+          result[0] = i;
+          result[1] = j;
+          return result;
+        }
+      }
+    }
+    return result;
+  }
+}
+/* Result Details
+Runtime: 33 ms, faster than 11.66% of Java online submissions for Two Sum.
+Memory Usage: 37.4 MB, less than 98.95% of Java online submissions for Two Sum.
+ */
